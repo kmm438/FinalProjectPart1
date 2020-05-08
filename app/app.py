aca83cc6d358f49a9d4fb581b209332518adbad1
Kimberly McDaniel <km438@njit.edu>
2020-05-08 12:30:50 +0100

    Feature: Editing BREAD methods on the app.py

--- app/app.py
+++ app/app.py
@@ -25,37 +25,37 @@ def index():
     return render_template('index.html', title='Home', user=user, hw=result)
 
 
-@app.route('/view/<int:city_id>', methods=['GET'])
-def record_view(city_id):
+@app.route('/view/<int:hw_id>', methods=['GET'])
+def record_view(hw_ic):
     cursor = mysql.get_db().cursor()
-    cursor.execute('SELECT * FROM bmiData WHERE id=%s', city_id)
+    cursor.execute('SELECT * FROM bmiData WHERE id=%s', hw_id)
     result = cursor.fetchall()
-    return render_template('view.html', title='View Form', city=result[0])
+    return render_template('view.html', title='View Form', hw=result[0])
 
 
-@app.route('/edit/<int:city_id>', methods=['GET'])
-def form_edit_get(city_id):
+@app.route('/edit/<int:hw_id>', methods=['GET'])
+def form_edit_get(hw_id):
     cursor = mysql.get_db().cursor()
-    cursor.execute('SELECT * FROM bmiData WHERE id=%s', city_id)
+    cursor.execute('SELECT * FROM bmiData WHERE id=%s', hw_id)
     result = cursor.fetchall()
-    return render_template('edit.html', title='Edit Form', city=result[0])
+    return render_template('edit.html', title='Edit Form', hw=result[0])
 
 
-@app.route('/edit/<int:city_id>', methods=['POST'])
-def form_update_post(city_id):
+@app.route('/edit/<int:hw_id>', methods=['POST'])
+def form_update_post(hw_id):
     cursor = mysql.get_db().cursor()
     inputData = (request.form.get('fldName'), request.form.get('fldLat'), request.form.get('fldLong'),
                  request.form.get('fldCountry'), request.form.get('fldAbbreviation'),
-                 request.form.get('fldCapitalStatus'), request.form.get('fldPopulation'), city_id)
+                 request.form.get('fldCapitalStatus'), request.form.get('fldPopulation'), hw_id)
     sql_update_query = """UPDATE bmiData t SET t.fldName = %s, t.fldLat = %s, t.fldLong = %s, t.fldCountry = 
     %s, t.fldAbbreviation = %s, t.fldCapitalStatus = %s, t.fldPopulation = %s WHERE t.id = %s """
     cursor.execute(sql_update_query, inputData)
     mysql.get_db().commit()
     return redirect("/", code=302)
 
-@app.route('/cities/new', methods=['GET'])
+@app.route('/hw/new', methods=['GET'])
 def form_insert_get():
-    return render_template('new.html', title='New City Form')
+    return render_template('new.html', title='New Height Weight Form')
 
 
 @app.route('/cities/new', methods=['POST'])
